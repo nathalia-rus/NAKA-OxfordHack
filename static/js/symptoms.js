87d61e27f044e9cdda042fa4b0b1dd3480db46e7
adelliinaa <33165052+adelliinaa@users.noreply.github.com>
2017-11-26 12:50:50 +0000

    Add files via upload
    Add symptoms

--- static/js/symptoms.js
+++ static/js/symptoms.js
@@ -1,2 +1,6 @@
-var symptoms = ['fever', 'cold', 'headache'];
+var symptoms = ['fever', 'cold', 'headache', 'muscle weakness', 'abdominal pain', 'weight gain', 'loss of appetite',  'can\'t breathe normally','abdominal pain', 'back pain', 'chest pain', 'earache', 'pelvic pain', 'toothache', 'vaginal pain', 'rectal pain', 'chills', 'light-headed','flu', 'nauseated','sick', 'sweaty', 'sweating', 'thirsty', 'tired', 'weakness', 'weak', 'loose hearing','blindliness','can\t pass urine normally', 'can\'t remember normally', 'scratching','can\'t swollow','can\'t write', 'can\'t walk','cough','bad breath','fatigue', 'runny nose','sneezing','mucus buildup','stuffy nose', 'sleepy','hallucinations','unusual cycle','elevated heart rate', 'intense alerteness' , 'discharge','bleeding','swelling' ,'sweats' , 'anomia' , 'anosognosia' , 'anxiety' , 'drug overdose' , 'depression' , 'dysarthria' , 'chorea' , 'claustrophobia' , 'confusion' , 'insomnia' , 'vaginal bleeding' , 'labour pains' , 'sore breasts' , 'sleep paralysis', 'loss of muscle control', 'blurred vision', 'short of breath', 'sounds too loud'];
 var class_styles = ['primary', 'secondary', 'success', 'danger', 'warning', 'info', 'dark'];
+'deformity','sweats' ]
+
+
+
